94144984f63f9d36e5b358aa4662f8b33d6bf9d5
Joel Henrique <104386844+Joel-Henrique@users.noreply.github.com>
2025-04-27 14:35:36 -0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      'no-unused-vars': 'off', // <<< Adiciona esta linha
+    },
+  },
 ];
 
 export default eslintConfig;
